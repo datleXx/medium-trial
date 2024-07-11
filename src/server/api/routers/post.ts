534ca4db2ff5394cf7d631cf74719b5c0289077f
datleXx <dat91fitzz@gmail.com>
2024-07-12 02:35:00 +0700

    ENV

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -19,7 +19,7 @@ import { PrismaClient } from "@prisma/client";
 // set AWS credential blog bucket
 
 const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 const bucketName = process.env.AWS_BUCKET_NAME;
 
